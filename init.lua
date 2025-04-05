9bd21d334646fe7d7ddd1436751a39ccf002a73f
BPernes <bernardopvsouza@gmail.com>
2025-04-05 12:30:19 -0300

    chore: netrw keybind

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ require("config.lazy")
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 --Global settings
 vim.opt.shiftwidth = 4
