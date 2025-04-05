aa76ab04c86947c557d1c90b8c75d1983e29c8b0
BPernes <bernardopvsouza@gmail.com>
2025-04-05 11:48:11 -0300

    feat: neovim opts per language

--- init.lua
+++ init.lua
@@ -6,8 +6,6 @@ vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
-vim.opt.shiftwidth = 2
-
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying text)',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
