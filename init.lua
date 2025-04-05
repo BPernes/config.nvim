443e0a4fb4838414624401345090d958f5370ccb
BPernes <bernardopvsouza@gmail.com>
2025-04-05 11:51:58 -0300

    chore: some global settings

--- init.lua
+++ init.lua
@@ -6,6 +6,11 @@ vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+--Global settings
+vim.opt.shiftwidth = 4
+vim.opt.relativenumber = true
+vim.opt.guicursor="n-v-c-i:block"
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying text)',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
