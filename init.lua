953891feed7b3f3b22fca6487d113d5767e9bdfc
BPernes <bernardopvsouza@gmail.com>
2025-04-06 10:01:19 -0300

    feat: quickfix navigation

--- init.lua
+++ init.lua
@@ -7,13 +7,16 @@ vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("n", "<C-A-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<C-A-k>", "<cmd>cprev<CR>")
+
 --Global settings
 vim.opt.shiftwidth = 4
 vim.opt.relativenumber = true
-vim.opt.guicursor="n-v-c-i:block"
+vim.opt.guicursor = "n-v-c-i:block"
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying text)',
+  desc = 'Highlight when yanking (coying text)',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
