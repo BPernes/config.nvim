f482c1a2e61dc7e54197a4bd6391cb5cbb215a5e
BPernes <bernardopvsouza@gmail.com>
2025-04-06 12:58:24 -0300

    chore: push keybind

--- lua/config/plugins/fugitive.lua
+++ lua/config/plugins/fugitive.lua
@@ -4,6 +4,7 @@ return {
     config = function()
       vim.keymap.set("n", "<space>gs", vim.cmd.Git)
       vim.keymap.set("n", "<space>gc", "<cmd>Git commit<CR>")
+      vim.keymap.set("n", "<space>gp", "<cmd>Git push<CR>")
     end
   }
 }
