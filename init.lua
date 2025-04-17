12e67384656737db17052bc8bbdff79bba4d6e16
BPernes <bernardopvsouza@gmail.com>
2025-04-17 11:32:23 -0300

    chore: moved global remaps to dedicated file

--- init.lua
+++ init.lua
@@ -1,20 +1,5 @@
+require("config.remap")
 require("config.lazy")
-
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
-vim.keymap.set("v", "<space>x", ":lua<CR>")
-vim.keymap.set("n", "<leader>pv", "<cmd>:e .<CR>")
-
--- QuickFix nav
-vim.keymap.set("n", "<C-A-j>", "<cmd>cnext<CR>")
-vim.keymap.set("n", "<C-A-k>", "<cmd>cprev<CR>")
-
-vim.keymap.set("n", "<space>v", "<cmd>vsplit<CR>")
-
---Global settings
-vim.opt.shiftwidth = 4
-vim.opt.relativenumber = true
-vim.opt.guicursor = "n-v-c-i:block"
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying text)',
---
+++ lua/config/remap.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- QuickFix nav
+vim.keymap.set("n", "<C-A-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<C-A-k>", "<cmd>cprev<CR>")
+
+vim.keymap.set("n", "<space>v", "<cmd>vsplit<CR>")
+vim.keymap.set("n", "<space>w", "<cmd>:w<CR>")
+vim.keymap.set("n", "-", "<cmd>Oil<CR>")
+
+--Global settings
+vim.opt.shiftwidth = 4
+vim.opt.relativenumber = true
+vim.opt.guicursor = "n-v-c-i:block"
